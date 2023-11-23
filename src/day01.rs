4429a4284053846afaf6676e98050f008b398e97
Daniel Below <daniel.below@tum.de>
2023-11-23 19:53:43 +0100

    Initial commit

---
+++ src/day01.rs
@@ -0,0 +1,19 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day01)]
+pub fn generate(_inp: &str) -> Vec<usize> {
+    todo!()
+}
+
+#[aoc(day01, part1)]
+pub fn part1(_inp: &[usize]) -> usize {
+    todo!()
+}
+
+#[aoc(day01, part2)]
+pub fn part2(_inp: &[usize]) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {}
